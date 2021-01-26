d9d13ad2b6cc1f8cf7bd925ef7715259e85b02f1
Erdinc Kaya <kaya@hpe.com>
2021-01-26 14:16:11 +0400

    adding vmware, need to update vmware specific parameters - updated

---
+++ src/VMWare.js
@@ -0,0 +1,174 @@
+import React from 'react';
+import { Spinning } from 'grommet-controls';
+import { Box, Button, CheckBox, Form, FormField, Heading, Layer, Text, TextInput } from 'grommet';
+import { sendError, sendOutput, runMultiCommand, installNeeded, readFromStore, commandToCheck, getCommandOutput, saveToStore, getPlatform, sendStatus } from './helpers';
+import { required } from './vmware_requires';
+import { Next, StatusGood, StatusWarning, Vmware } from 'grommet-icons';
+import Target from './host_target';
+
+export const VMWare = () => {
+  const [ ready, setReady ] = React.useState(false);
+  const [ host, setHost ] = React.useState({});
+  const [ targetready, setTargetready ] = React.useState(false);
+  const [ loading, setLoading ] = React.useState(false);
+  const [ commands, setCommands ] = React.useState([]);
+  const [ epicurl, setEpicurl ] = React.useState('');
+  const repourl = 'https://github.com/erdincka/ezmeral-demo-vmware-terraform.git'; // TODO: Read from Platforms.url
+  const repodir = './ezmeral-demo-vmware-terraform';
+  const tfcommand = (cmd) => 'TF_IN_AUTOMATION=true terraform ' + cmd + ' -no-color -input=false '
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      let vmware = JSON.parse(await readFromStore('vmware'));
+      let host = JSON.parse(await readFromStore('host'));
+      if ( vmware.epicurl ) setEpicurl(vmware.epicurl);
+      setHost(host);
+    };
+    fetchData();
+  }, []);
+
+  const checkTarget = (status) => {
+    if (status) {
+      // Check if requirements are available
+      let cmds = [];
+      required.forEach( group => group.needs.forEach( need => cmds.push(commandToCheck(need)) ) );
+      setLoading(true);
+      runMultiCommand(cmds)
+      .then(result => {
+        let [ out, err ] = getCommandOutput(result);
+        if (err) sendError(err) && setLoading(false);
+        if (out) {
+          sendOutput(out);
+          let found = [];
+          out.split('\n').forEach(line => {
+            if (line.match(/^\/\w+/)) // if path starts with / (returned from which command)
+            found.push(line.trim().split('/').pop()); // extract command name from path
+          });
+          setCommands(found);
+          setLoading(false);
+        }
+      });
+    }
+    setTargetready(status);
+  }
+
+  const prepare = async (c) => {
+    setLoading(true); // TODO: This is not working as expected
+    saveToStore('vmware.epicurl', JSON.stringify(epicurl));
+    sendStatus('vmware settings are saved');
+    let commands = [
+      '[ -d ' + repodir + ' ] || git clone ' + repourl + ' ' + repodir,
+      'pushd ' + repodir + ' > /dev/null', // enter the repodir
+      // 'cp ./etc/postcreate.sh_template ./etc/postcreate.sh',
+      'sed -i \'s|^epic_dl_url.*=.*$|epic_dl_url = "' + epicurl.replace(/\&/g, '\\&') + '"|\' ./etc/bluedata_infra.tfvars', // escape url string with |
+    ];
+    commands.push('echo tfvars updated');
+    commands.push(tfcommand('init'));
+    commands.push('popd > /dev/null'); // exit the repodir
+    runMultiCommand(commands)
+      .then(result => {
+        sendOutput(result.stdout);
+        if (result.stderr) sendError(result.stderr);
+        else setReady(true);
+      })
+      .catch(err => sendError(err.message));
+
+    setLoading(false);
+  }
+
+  const verifyNeed = async (n) => {
+    const need = JSON.parse(n);
+    setLoading(true);
+    let result = await installNeeded(need);
+    setLoading(false);
+    if (result) {
+      setCommands(old => [...old, need.command]);
+    }
+  }
+
+  const deploy = () => {
+    let commands = [
+      'pushd ' + repodir + ' > /dev/null',
+      // workaround for my M1 MacOS
+      'PATH="${PATH}":"$(python3 -m site --user-base)/bin" ./bin/vmware_create_new.sh',
+      'popd > /dev/null'
+    ]
+    runMultiCommand(commands)
+      .then(result => {
+        sendOutput(result.stdout);
+        if (result.stderr) sendError(result.stderr)
+      })
+      .catch(error => sendError(error.message));
+  }
+
+  const updateHost = async (isremote) => {
+    await saveToStore('host.isremote', JSON.stringify(isremote));
+    setHost(prev => ({ ...prev, 'isremote': isremote }));
+    checkTarget(true);
+  }
+
+  return (
+    <Box gap='small' pad='xsmall' fill flex={false}>
+    { loading && <Layer animation='fadeIn' onEsc={ setLoading(false) } ><Spinning size='large' /></Layer> }
+    <CheckBox 
+      checked={ host.isremote }
+      onChange={ (event) => updateHost(event.target.checked) }
+      label={ host.isremote ? 'Remote' : 'Local' }
+      toggle
+    />
+    { host.isremote && <Target setParent={ (res) => updateHost(res) } /> }
+    {
+        targetready && required.map(req => 
+          <Box key={req.group} pad='xsmall'>
+            <Heading level='5' margin='none' color='neutral-2'>{req.group}</Heading>
+            { 
+              req.needs && req.needs.map( need => 
+                <Box margin='none' direction='row' key={ need.command } justify='between' align='center' >
+                  <Text >{ need.command }</Text>
+                  <Box direction='row' align='center'>
+                    <Button 
+                      disabled={ loading || commands.includes(need.command) } 
+                      label={ commands.includes(need.command) ? 'Ready' : 'Install' }
+                      color={ commands.includes(need.command) ? '' : 'plain' }
+                      id={ JSON.stringify(need) }
+                      onClick={ event => verifyNeed(event.target.id) }
+                    />
+                    { commands.includes(need.command) ? <StatusGood color='status-ok' /> : <StatusWarning color='status-warning' />}
+                  </Box>
+                </Box>
+              )
+            }
+          </Box>
+        )
+      }
+
+{ // display if all requirements are met
+      (required.map(req => req.needs.map(n => n.command)).flat().length === commands.length) &&
+      <Box>
+        <FormField name='epicurl' htmlfor='epicurl' label='EPIC Download URL' required >
+          <TextInput id='epicurl' name='epicurl' value={ epicurl } onChange={ event => setEpicurl(event.target.value ) } />
+        </FormField>
+        <Button label='Prepare' 
+          secondary 
+          hoverIndicator 
+          icon={ <Next /> } reverse 
+          disabled={ ! epicurl }
+          onClick={ () => prepare() }
+        />
+
+      </Box>
+      }
+
+      {
+        ready && 
+        <Button 
+          onClick={ deploy }
+          type='button' 
+          color='plain' 
+          label='Deploy on vSphere' 
+          icon={ <Vmware /> } />
+
+      }
+    </Box>
+  )
+}
---
+++ src/host_target.js
@@ -0,0 +1,162 @@
+import React from 'react';
+import { Box, Button, CheckBox, Form, FormField, TextInput } from 'grommet';
+import { Next } from 'grommet-icons';
+import { PasswordInput } from 'grommet-controls';
+import { sendStatus, sendError, sendOutput, runCommand, readFromStore, getPlatform, canSsh, saveToStore } from './helpers';
+import { defaultHost } from './defaultHost';
+
+function Target(props) {
+  const { setParent } = props;
+
+  const [platform, setPlatform] = React.useState();
+  const [host, setHost] = React.useState(defaultHost);
+  
+  React.useEffect(() => {
+    const fetchData = async () => {
+      // get host from stored settings
+      const stored = JSON.parse(await readFromStore('host'));
+      if (stored.isremote) setHost(stored);
+      // else console.dir(defaultHost);
+      // set the platform we operate on
+      setPlatform(await getPlatform());
+    };
+    fetchData();
+  }, []);
+
+  const saveHost = () => {
+    host.isremote ?
+      canSsh()
+      .then(res => {
+        // save settings
+        saveToStore('host', JSON.stringify(host))
+        .then( () => {
+          // check if connection to remote host successful
+          runCommand('uname -a')
+            .then( (res) => {
+              if (res && res.stderr === '') {
+                sendStatus('Connected to ' + host.username + '@' + host.hostname);
+                sendOutput(res.stdout);
+                setParent(true);
+              }
+              else sendError(res.stderr);
+            })
+            // catch error with ssh connection
+            .catch(error => sendError(error.message.replace('Error invoking remote method \'get-system\':', '')));
+            }
+          )
+          // catch error with setting store key
+          .catch(error => sendError(error.message.replace('Error invoking remote method \'set-store-value\':', ''))
+          );
+      })
+      // catch error with ssh command (if exists)
+      .catch(error => sendError(error.message.replace('Error invoking remote method \'get-system\':', ''))
+      )
+    : // if local deployment
+      saveToStore('host', JSON.stringify(host))
+      .then( () => {
+        sendStatus('Target set to localhost');
+        setParent(true);
+      })
+      .catch(error => sendError(error.message.replace('Error invoking remote method \'get-system\':', '')));
+  }
+
+  const proxyBox = 
+  <Box>
+    <Box direction='row'>
+      <FormField name='proxyhostname' htmlfor='proxyhost-id' label='Proxy Hostname' required>
+        <TextInput id='proxyhost-id' name='proxyhostname' placeholder='hostname / ip address' />
+      </FormField>
+      <FormField name='proxyusername' htmlfor='proxyname-id' label='Username' required>
+        <TextInput id='proxyname-id' name='proxyusername' placeholder='username' />
+      </FormField>
+    </Box>
+    <CheckBox 
+      name='useproxykeyfile'
+      label="Use private key file?"
+      toggle
+      checked={host.useproxykeyfile}
+    />
+    <Box> {
+        host.useproxykeyfile ? 
+          <FormField name='proxykeyfile' htmlfor='proxykeyfile-id' label='Private SSH Key File' required>
+            <TextInput id='proxykeyfile-id' name='proxykeyfile' placeholder='path to private key file' />
+          </FormField>
+        :
+          <FormField name='proxypassword' htmlfor='proxypass-id' label='Password' required>
+            <PasswordInput id='proxypass-id' name='proxypassword' placeholder='password' />
+          </FormField>
+      } </Box>
+    </Box>;
+
+  const hostBox = 
+    <Box>
+      <Box direction='row'>
+        <FormField name='hostname' htmlfor='host-id' label='Hostname' required>
+          <TextInput id='host-id' name='hostname' placeholder='hostname / ip address' />
+        </FormField>
+        <FormField name='username' htmlfor='name-id' label='Username' required>
+          <TextInput id='name-id' name='username' placeholder='username' />
+        </FormField>
+      </Box>
+      <CheckBox 
+        name='usekeyfile'
+        label="Use private key file?"
+        toggle
+        checked={host.usekeyfile}
+      />
+      <Box>
+        { host.usekeyfile ? 
+          <FormField name='keyfile' htmlfor='keyfile-id' label='Private SSH Key File' required>
+            <TextInput id='keyfile-id' name='keyfile' placeholder='path to private key file' />
+          </FormField>
+          :
+          <FormField name='password' htmlfor='pass-id' label='Password' required>
+            <PasswordInput id='pass-id' name='password' placeholder='password' />
+          </FormField>
+        }
+      </Box>
+    </Box>;
+
+  return(
+    <Box pad='small' flex>
+      {/* { JSON.stringify(host) } <br /> */}
+      <Form direction='row'
+        value={ host }
+        validate='submit'
+        onChange={ nextValue => setHost(nextValue) }
+        onSubmit={ () => saveHost() }
+      >
+        <Box direction='row'>
+          { platform === 'linux' && <CheckBox 
+            // disabled={ platform === 'linux' ? false : true }
+            name='isremote'
+            label={ host.isremote ? 'Remote' : 'Local' }
+            toggle
+            checked={ host.isremote || true }
+          />
+          }
+          { platform !== 'linux' && <CheckBox 
+            name='useproxy'
+            label="Via proxy?"
+            toggle
+            checked={host.useproxy}
+          />}
+        </Box>
+        { platform !== 'linux' &&
+          <Box>
+            { host.useproxy && proxyBox
+            }
+            { hostBox }
+          </Box>
+        }
+        <Box direction='row' gap='medium' justify='end'>
+          <Button type='submit' label='Next' icon={ <Next /> }
+            disabled={ !host }
+          />
+        </Box>
+      </Form>
+    </Box>
+  )
+}
+
+export default Target;
